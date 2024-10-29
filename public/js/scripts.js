471cd38bd4d9877007f4b2d01b8c531520764495
AnaghMiglani <150614061+AnaghMiglani@users.noreply.github.com>
2024-10-29 19:04:12 +0530

    Added sharing through social media

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,7 @@
+let uniqueLink;
 document.getElementById('greetingForm').onsubmit = async function (event) {
     event.preventDefault();
-  
+
     const formData = new FormData(this);
 
     try {
@@ -12,12 +13,16 @@ document.getElementById('greetingForm').onsubmit = async function (event) {
         const data = await response.json();
 
         if (response.ok) {
-            const uniqueLink = `${window.location.origin}/greeting/${data.uniqueId}`;
+            uniqueLink = `${window.location.origin}/greeting/${data.uniqueId}`;
             console.log('Your greeting link:', uniqueLink);
 
             document.getElementById('result').innerHTML = `
                 <p>Your greeting has been created! Share this link:</p>
                 <a href="${uniqueLink}" target="_blank">${uniqueLink}</a>
+                <br>
+                <button onclick="shareOnFacebook()">Share on Facebook</button>
+                <button onclick="shareOnTwitter()">Share on Twitter</button>
+                <button onclick="shareOnWhatsApp()">Share on WhatsApp</button>
             `;
         } else {
             document.getElementById('result').innerHTML = `<p>Error: ${data.error || 'Unknown error'}</p>`;
@@ -27,3 +32,24 @@ document.getElementById('greetingForm').onsubmit = async function (event) {
         console.error('Error uploading greeting:', error);
     }
 };
+
+function shareOnFacebook() {
+    console.log('Facebook share button clicked');
+    const url = encodeURIComponent(uniqueLink);
+    const message = encodeURIComponent('Check out my Diwali greeting!');
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}&quote=${message}`, '_blank');
+}
+
+function shareOnTwitter() {
+    console.log('Twitter share button clicked');
+    const url = encodeURIComponent(uniqueLink);
+    const message = encodeURIComponent('Check out my Diwali greeting!');
+    window.open(`https://twitter.com/intent/tweet?url=${url}&text=${message}`, '_blank');
+}
+
+function shareOnWhatsApp() {
+    console.log('WhatsApp share button clicked');
+    const url = encodeURIComponent(uniqueLink);
+    const message = encodeURIComponent('Check out my Diwali greeting!');
+    window.open(`https://wa.me/?text=${message} ${url}`, '_blank');
+}
